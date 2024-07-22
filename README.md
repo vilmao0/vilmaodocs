39b6bfdf26268e15664db39c8d67a78d6409dc37
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-07-22 14:27:25 +0200

    REALNIGGERTATE1

---
+++ README.md
@@ -0,0 +1,7 @@
+This is ... 
+
+## Getting Started
+
+Head to [WEB](https://vilmaodocs.vercel.app/) OR to the [DOWNLOAD](ftp://skibidi.nnn/dfdsf/R_N_T)
+
+&copy; copyright 2024 VilmaoGlobal
---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: 'VilmaoDocs',
+  description: 'Your go-to collaborative document editor',
+}
+
+export default function RootLayout({ children }:  {children: React.ReactNode}) {
+  return (
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+      variables: { 
+        colorPrimary: "#3371FF" ,
+        fontSize: '16px'
+      },
+    }}
+    >
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+    </ClerkProvider>
+  )
+}
