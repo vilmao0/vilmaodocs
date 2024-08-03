df545a02cfcb8732dc469c05abfa7841247f9a39
vilmao0 <168273738+vilmao0@users.noreply.github.com>
2024-08-03 19:20:10 +0200

    Update CollaborativeRoom.tsx

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -14,6 +14,7 @@ const CollaborativeRoom = ({ children }:  {children: React.ReactNode}) => {
                     <div className="flex w-fit items-center justify-center gap-2">
                     <p className='document-title'>SHARE</p>
                     </div>
+                </Header>
             </div>
             <SignedOut>
                 <SignInButton />
